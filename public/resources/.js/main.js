af67f1513dd024acce4eb7b99bde04a545889c7b
Ryan Howell <ryan@theryanhowell.co.uk>
2014-03-08 14:59:11 +0000

    Cleaning up

--- public/resources/.js/main.js
+++ public/resources/.js/main.js
@@ -1,12 +1,12 @@
 console.log('Main');
 
 if(geo_position_js.init()){
-    var success_callback = function (a, b ,c){
-        console.log('Success', a, b, c);
+    var success_callback = function (a){
+        console.log('Success', a);
     };
 
-    var error_callback = function (a, b ,c){
-        console.log('Failure', a, b, c);
+    var error_callback = function (a){
+        console.log('Failure', a);
     };
 
     geo_position_js.getCurrentPosition(success_callback,error_callback);
