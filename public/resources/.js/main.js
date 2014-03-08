debb0305086b62bb927e2ec097a59be1534b3f0f
Ryan Howell <ryan@theryanhowell.co.uk>
2014-03-08 15:13:22 +0000

    Storing in variable

--- public/resources/.js/main.js
+++ public/resources/.js/main.js
@@ -1,15 +1,18 @@
 console.log('Main');
 
 if(geo_position_js.init()){
-    var success_callback = function (a){
-        console.log('Success', a);
-    };
+    window.coordinates = false;
+    window.geoInterval = setInterval(function(){
+        var success_callback = function (a){
+            window.coordinates = a.coords;
+        };
 
-    var error_callback = function (a){
-        console.log('Failure', a);
-    };
+        var error_callback = function (a){
+            console.log('Failure', a);
+        };
+        geo_position_js.getCurrentPosition(success_callback,error_callback);
+    },10000);
 
-    geo_position_js.getCurrentPosition(success_callback,error_callback);
 }
 else{
     alert("Functionality not available");
