eed5a8c2ff8aa4655c0f61fd09df9c4207b6017a
Michael Craddock <michael@moblimic.com>
2014-03-08 13:22:31 +0000

    testing 1 2 testing

--- public/index.php
+++ public/index.php
@@ -1,27 +1,48 @@
 <?php
-	require_once('../.core/core.class.php');
-	$pageSettings = [
-		'views' => [
-			'header' => [
-				'pageName' => 'Home',
-				'sass'     => ['main' => true],
-				'js'       => ['main' => true]
-			],
-			'body'   => true,
-			'footer' => true
-		],
-		'forms' => true
-	];
-	$thisPage = new HC_Page($pageSettings);
+require_once('../.core/core.class.php');
+$pageSettings = [
+    'views' => [
+        'header' => [
+            'pageName' => 'Home',
+            'sass' => ['main' => true],
+            'js' => ['main' => true]
+        ],
+        'body' => true,
+        'footer' => true
+    ],
+    'forms' => true
+];
+$thisPage = new HC_Page($pageSettings);
 
-	// Render Header
-	$thisPage->renderView('header');
+// Render Header
+$thisPage->renderView('header');
 ?>
 
+    <div class="container">
 
+        <div class="row">
+
+            <div class="col-md-3">
+                <span class="glyphicons-icon fast_food"></span>
+            </div>
+
+            <div class="col-md-3">
+
+            </div>
+
+            <div class="col-md-3">
+
+            </div>
+
+            <div class="col-md-3">
+
+            </div>
+
+        </div>
+    </div>
 
 
 <?php
-	// Render Footer
-	$thisPage->renderView('footer');
+// Render Footer
+$thisPage->renderView('footer');
 ?>
