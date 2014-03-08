4b8549b00c946e01651252e304ce7df217a45cc0
Ryan Howell <ryan@theryanhowell.co.uk>
2014-03-08 12:16:02 +0000

    Components

--- .views/footer.php
+++ .views/footer.php
@@ -1,13 +1,6 @@
 </section>
 <footer>
 	<?php
-		if (ENVIRONMENT == 'DEV') {
-			$endTime    = microtime();
-			$endTime    = explode(' ', $endTime);
-			$endTime    = $endTime[1] + $endTime[0];
-			$total_time = round(($endTime - $GLOBALS['HC_CORE']->getSite()->getStartTime()), 4);
-			echo '<small>Page generated in ' . $total_time . ' seconds.</small><br>';
-		}
 		echo '<small>' . AUTHOR . '</small>';
 	?>
 </footer>
