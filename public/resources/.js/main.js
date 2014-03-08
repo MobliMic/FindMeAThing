cbda20994a3833f6de988238c5d54153f55f4c3f
Ryan Howell <ryan@theryanhowell.co.uk>
2014-03-08 14:56:11 +0000

    callbacks

--- public/resources/.js/main.js
+++ public/resources/.js/main.js
@@ -1,6 +1,14 @@
 console.log('Main');
 
 if(geo_position_js.init()){
+    var success_callback = function (a, b ,c){
+        console.log('Success', a, b, c);
+    };
+
+    var error_callback = function (a, b ,c){
+        console.log('Failure', a, b, c);
+    };
+
     geo_position_js.getCurrentPosition(success_callback,error_callback);
 }
 else{
