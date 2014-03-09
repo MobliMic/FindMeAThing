fc9ccf187609d60a7a36a592e45591594cec73e5
Michael Craddock <michael@moblimic.com>
2014-03-09 13:28:46 +0000

    work now please

--- public/resources/.js/food.js
+++ public/resources/.js/food.js
@@ -3,9 +3,12 @@
  */
 
 
-function updateFoodList(){
+function updateFoodList() {
 
-
+    $.each(nearBusiness(), function (index, value) {
+        console.log(index);
+        console.warn(value);
+    });
 
 }
 
